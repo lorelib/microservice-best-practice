d37a67f2700ccd304fad23e54253f9f0e321d846
listening <893979653@qq.com>
2017-01-04 16:04:55 +0800

    使用spring boot admin管理spring cloud应用

---
+++ infrastructure/spring-boot-admin/src/main/java/com/lorelib/microservice/springboot/admin/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.lorelib.microservice.springboot.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by listening on 2017/1/4.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
